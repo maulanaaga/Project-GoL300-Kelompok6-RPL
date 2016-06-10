1606d2a4c4cc68fcd225d07d03cb421f0c0bb307
maulanaaga <agamaulana.informatika@gmail.com>
2016-06-11 02:23:01 +0700

    update jadwal keberangkatan untuk admin

--- admin/laporan.php
+++ admin/laporan.php
@@ -219,6 +219,7 @@ $queryString_rsjadwal = sprintf("&totalRows_rsjadwal=%d%s", $totalRows_rsjadwal,
 $nextWeek = time() + (7*24*60*60);//7days;24 hours;60 mins;60 secs
 echo 'SEKARANG: '.date('d-m-Y')."\n";
 echo 'MINGGU DEPAN:'.date('d-m-Y',$nextWeek)."\n";
+
 ?>
   </h10></p>
           <p align="center" class="style2">LAPORAN TRANSAKSI</p>
