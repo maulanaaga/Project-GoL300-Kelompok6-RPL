c9aec817d91a251c7bcaaef1d0434a412b2f8c04
maulanaaga <agamaulana.informatika@gmail.com>
2016-06-11 03:37:21 +0700

    update jadwal edit php

--- admin/jadwal_edit.php
+++ admin/jadwal_edit.php
@@ -212,7 +212,7 @@ $queryString_rsjadwal = sprintf("&totalRows_rsjadwal=%d%s", $totalRows_rsjadwal,
 <div id="container">
 	<div id="header">
     	<h1><a href="/"><img src="../images/footer-l.png" alt="" width="94" height="89" longdesc="images/200px-PT.KA.svg.png" /></a></h1>
-      <h2>Welcome to ticket On-Line Kereta Api</h2>
+      <h2>Welcome to GO-L300</h2>
         <div class="clear"></div>
     </div>
 <div id="nav">
@@ -224,8 +224,8 @@ $queryString_rsjadwal = sprintf("&totalRows_rsjadwal=%d%s", $totalRows_rsjadwal,
     </ul>
   </div>
     <div id="page-intro">
-    	<h2>Welcome to ticket On-Line Kereta Api</h2>
-        <p>Membuat kemudahan dan kenyamanan dalam pemesanan tiket kereta api, "NO COPET, NO NGANTRI, NO CALO"</p>
+    	<h2>Welcome to ticket GO-L300</h2>
+        <p>Membuat kemudahan dan kenyamanan dalam pemesanan Ticket L300, "NO REPOT, NO NGANTRI, NO CALO"</p>
         <p>&nbsp;</p>
   </div>
     <div id="body">
