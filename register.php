73b71f2c49a4621fa800a07a20ccabea40c41c97
Mukhsin Arafat <mukhsin@outlook.com>
2016-06-11 03:30:35 +0700

    edit register.php

--- register.php
+++ register.php
@@ -2,3 +2,88 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <title>Welcome To Kereta On-Line</title>
+<link rel="Shortcut Icon" href="images/train.ico">
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+<script type="text/javascript" src="js/jquery00.js"></script> 
+<script type="text/javascript" src="js/jquery01.js"></script>		
+<script type="text/javascript" src="js/coin-sli.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){$('#coin-slider').coinslider({ 
+width:900,height:300,navigation:true,effect:'random',//random, swirl, rain, straight
+delay:3000//Delay between transitions
+});});
+</script>
+<script type="text/javascript" src="js/function.js"></script>
+<style type="text/css">
+<!--
+.style1 {
+	color: #FF0000;
+	font-weight: bold;
+}
+.style2 {color: #FF0000}
+-->
+</style>
+</head>
+<body class="noJs">
+<div class="container">
+		<div id="header">
+			<div class="wrap">
+				<div class="logo">
+					<a href="#" title="Design Lovers">Design Lovers</a>
+				</div>
+				<ul id="nav">
+					<li class="current"><a href="index.php">Home</a></li>
+				  <li><a href="jadwal1.php">jadwaL</a></li>
+                    <li><a href="berita1.php">berita</a></li>
+				  <li><a href="arsip1.php">ARSIP</a></li>
+				  <li><a href="contact1.php">Contact</a></li>
+			  </ul>
+			</div>
+		</div>
+		<div class="separator"></div>
+		<div id="slider_container">
+			<div class="wrap">
+				<div id="coin-slider">
+					<a href="#">
+						<img src="images/f1.jpg" alt="" />
+						<span>Welcome To Kereta On-Line</span>
+					</a>
+					<a href="#">
+						<img src="images/f2.jpg" alt="" />
+						<span><strong>Argo Bromo.</strong> Jakarta-Surabaya, hanya 5 jam. </span>
+					</a>
+					<a href="#">
+						<img src="images/f3.jpg" alt="" />
+						<span><strong>Pemandangan yang indah</strong> Nikmati perjalanan dengan pemandangan yang asri dan nyaman </span>
+					</a>
+					<a href="#">
+						<img src="images/f4.jpg" alt="" />
+						<span><strong>Kami Siap melayani anda.</strong> Cepat, Tepat, Aman , dan terjangkau </span>
+					</a>
+					<a href="#">
+						<img src="images/f5.jpg" alt="" />
+						<span><strong>Interior yang Nyaman</strong> kami manjakan anda dengan segala fasilitas yang membuat anda nyaman </span>
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="separator"></div>
+		<div class="columns">
+			<div class="wrap">
+				<div class="oneOfThree first">
+				  <div id="log">
+                    <h3>pendaftaran Pengguna</h3>
+				    
+                    <form action="<?php echo $editFormAction; ?>" method="post" name="form1" id="form1">
+                      <table width="109%" align="center">
+                        <tr valign="baseline">
+                          <td nowrap="nowrap" align="right">Username:</td>
+                          <td><input type="text" name="username" value="" size="20" /></td>
+                        </tr>
+                        <tr valign="baseline">
+                          <td nowrap="nowrap" align="right">Password:</td>
+                          <td><input type="text" name="password" value="" size="20" /></td>
+                        </tr>
