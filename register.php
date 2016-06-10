16c83d3dfbea7a97add5503d9b66190062fb2eea
Mukhsin Arafat <mukhsin@outlook.com>
2016-06-11 03:54:14 +0700

    Update register.php

--- register.php
+++ register.php
@@ -101,7 +101,7 @@ if (isset($_POST['text1'])) {
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title>Welcome To Kereta On-Line</title>
+<title>Welcome To GO-L300</title>
 <link rel="Shortcut Icon" href="images/train.ico">
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <meta name="keywords" content="" />
