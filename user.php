9874f7aa7658e1bb70fb16a1de5a80b2db26714c
Mukhsin Arafat <mukhsin@outlook.com>
2016-06-11 01:33:41 +0700

    Membuat form feedback + link ke halaman pemesanan tiket

--- user.php
+++ user.php
@@ -139,3 +139,109 @@ delay:3000//Delay between transitions
 -->
 </style>
 </head>
+<body class="noJs">
+	<div class="container">
+		<div id="header">
+			<div class="wrap">
+				<div class="logo">
+					<a href="user.php" title="Design Lovers">Design Lovers</a>				</div>
+		  <ul id="nav">
+					<li class="current"><a href="user.php">Home</a></li>
+				  <li><a href="jadwal.php">JADWAL</a>					</li>
+		    <li><a href="berita.php">BERITA</a></li>
+			<li><a href="contact.php">Contact</a></li>
+			  </ul>
+			</div>
+		</div>
+		<div class="separator"></div>
+		<div class="wrap">
+				<div class="main intro">Membuat kemudahan dan kenyamanan dalam pemesanan tiket L300, &quot;NO NGANTRI, NO CALO, GA PAKE RIBET&quot;</div>
+				
+  <div class="sidebar" id="search_box">
+    <div align="center"><?php 
+$nextWeek = time() + (7*24*60*60);//7days;24 hours;60 mins;60 secs
+echo 'SEKARANG: '.date('Y-m-d')."\n";
+echo 'MINGGU DEPAN:'.date('Y-m-d',$nextWeek)."\n";
+?>
+  </h10>
+          <input type="submit" value="" id="search_btn" class="submit" />
+                                      </div>
+  </div>
+	  </div>
+			<div class="separator"></div>
+			<div class="wrap">
+			<div id="main" class="main">
+				<div class="headline">
+					<h1>Selamat Datang Anggota</h1>
+					<p>Dengan menjadi anggota, nikmati segala kemudahan yang ada di situs GO-L300.</p>
+				</div>
+				<div class="post">
+				  <h2 align="center"><a href="<?php echo $logoutAction ?>"><img src="images/119709197585381818TzeenieWheenie_Power_On_Off_Switch_red_2.svg.med.png" width="55" height="54" longdesc="images/119709197585381818TzeenieWheenie_Power_On_Off_Switch_red_2.svg.med.png" onclick="return confirmLogout()" /></a></h2>
+<blockquote> 
+                        <p align="center">Pesan tiket sekarang</p>
+            <p align="center"><a href="jadwal.php"><img src="images/1194986423899936796telefono_email_frolland_01.svg.med.png" width="116" height="103" longdesc="images/phone.png" /></a></p>
+				  </blockquote> 
+
+                    <p>&nbsp;</p>
+                    
+              </div>
+			</div>
+<div id="sidebar" class="sidebar">
+				<h2>Layanan Kami</h2>
+				<ul class="arrow_list">
+					<li><a href="jadwal.php">Pesan Tiket</a></li>
+				  <li><a href="search.php">Tracking Tiket</a></li>
+				  <li><a href="promo.php">Promo</a></li>
+	  </ul>
+				
+	  <div class="separator"></div>
+				
+	  <h2>Feedback</h2>
+				<h4>Saran anda membangun kami</h4>
+				
+                <form action="<?php echo $editFormAction; ?>" method="post" name="form1" id="form1">
+                  <table align="center">
+                    <tr valign="baseline">
+                      <td nowrap="nowrap" align="right">Username:</td>
+                      <td><input type="text" name="username" value="" size="25" /></td>
+                    </tr>
+                    <tr valign="baseline">
+                      <td nowrap="nowrap" align="right" valign="top">Pesan:</td>
+                      <td><textarea name="pesan" cols="25" rows="5" class="comment_meta"></textarea>
+                      </td>
+                    </tr>
+                    <tr valign="baseline">
+                      <td nowrap="nowrap" align="right">&nbsp;</td>
+                      <td><input type="submit" value="Kirim" />
+                      <input name="Reset" type="reset" value="Reset" /></td>
+                    </tr>
+                  </table>
+                  <input type="hidden" name="MM_insert" value="form1" />
+                </form>
+                <p>&nbsp;</p>
+</div>
+	  </div>
+	</div>
+	<div id="footer">
+		<div class="container">
+			<div class="wrap">
+				<div class="footer_logo">
+					<a href="user.php" title="Design Lovers">Design Lovers</a>				</div>
+		  <ul id="footer_nav">
+					<li class="current"><a href="user.php">Home</a></li>
+				  <li><a href="jadwal.php">JADWAL</a></li>
+			<li><a href="berita.php">BERITA</a></li>
+			<li><a href="contact.php">Contact</a></li>
+			  </ul>
+				<p id="copy">INF14 &copy;RPL-6</p>
+			</div>
+		</div>
+		<div class="clear"></div>
+	</div>
+</body> 
+</html>
+<?php
+mysql_free_result($rsusers);
+
+mysql_free_result($rstesti);
+?>
