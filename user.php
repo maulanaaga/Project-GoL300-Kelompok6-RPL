a58244653a464e7a74e2e332b6432bc392f1c757
Mukhsin Arafat <mukhsin@outlook.com>
2016-06-11 01:30:49 +0700

    Membuat menu header + menu pilihan layanan

--- user.php
+++ user.php
@@ -27,3 +27,115 @@ if ((isset($_GET['doLogout'])) &&($_GET['doLogout']=="true")){
   }
 }
 ?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? "'" . doubleval($theValue) . "'" : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+$editFormAction = $_SERVER['PHP_SELF'];
+if (isset($_SERVER['QUERY_STRING'])) {
+  $editFormAction .= "?" . htmlentities($_SERVER['QUERY_STRING']);
+}
+
+if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "form1")) {
+  $insertSQL = sprintf("INSERT INTO testi (username, pesan) VALUES (%s, %s)",
+                       GetSQLValueString($_POST['username'], "text"),
+                       GetSQLValueString($_POST['pesan'], "text"));
+
+  mysql_select_db($database_connkereta, $connkereta);
+  $Result1 = mysql_query($insertSQL, $connkereta) or die(mysql_error());
+
+  $insertGoTo = "testi_success.php";
+  if (isset($_SERVER['QUERY_STRING'])) {
+    $insertGoTo .= (strpos($insertGoTo, '?')) ? "&" : "?";
+    $insertGoTo .= $_SERVER['QUERY_STRING'];
+  }
+  header(sprintf("Location: %s", $insertGoTo));
+}
+
+mysql_select_db($database_connkereta, $connkereta);
+$query_rsusers = "SELECT * FROM users";
+$rsusers = mysql_query($query_rsusers, $connkereta) or die(mysql_error());
+$row_rsusers = mysql_fetch_assoc($rsusers);
+$totalRows_rsusers = mysql_num_rows($rsusers);
+
+$maxRows_rstesti = 3;
+$pageNum_rstesti = 0;
+if (isset($_GET['pageNum_rstesti'])) {
+  $pageNum_rstesti = $_GET['pageNum_rstesti'];
+}
+$startRow_rstesti = $pageNum_rstesti * $maxRows_rstesti;
+
+$colname_rstesti = "-1";
+if (isset($_GET['testiID'])) {
+  $colname_rstesti = $_GET['testiID'];
+}
+mysql_select_db($database_connkereta, $connkereta);
+$query_rstesti = sprintf("SELECT * FROM testi WHERE testiID = %s ORDER BY testiID DESC", GetSQLValueString($colname_rstesti, "int"));
+$query_limit_rstesti = sprintf("%s LIMIT %d, %d", $query_rstesti, $startRow_rstesti, $maxRows_rstesti);
+$rstesti = mysql_query($query_limit_rstesti, $connkereta) or die(mysql_error());
+$row_rstesti = mysql_fetch_assoc($rstesti);
+
+if (isset($_GET['totalRows_rstesti'])) {
+  $totalRows_rstesti = $_GET['totalRows_rstesti'];
+} else {
+  $all_rstesti = mysql_query($query_rstesti);
+  $totalRows_rstesti = mysql_num_rows($all_rstesti);
+}
+$totalPages_rstesti = ceil($totalRows_rstesti/$maxRows_rstesti)-1;
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Selamat datang Anggota</title><script>  
+function confirmLogout() {  
+        return confirm("Yakin Logout?")  
+      }  
+</script><link rel="Shortcut Icon" href="images/train.ico">  
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+<script type="text/javascript" src="js/jquery00.js"></script> 
+<script type="text/javascript" src="js/jquery01.js"></script>		
+<script type="text/javascript" src="js/coin-sli.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){$('#coin-slider').coinslider({ 
+width:900,height:300,navigation:true,effect:'random',//random, swirl, rain, straight
+delay:3000//Delay between transitions
+});});
+</script>
+<script type="text/javascript" src="js/function.js"></script>
+<style type="text/css">
+<!--
+.style1 {
+	color: #FF0000;
+	font-weight: bold;
+}
+-->
+</style>
+</head>
