e11b9efa7c7d62a6d1faade4d18beb5d83cb6d4b
Mukhsin Arafat <mukhsin@outlook.com>
2016-06-11 03:42:27 +0700

    Update halaman pendaftaran

--- register.php
+++ register.php
@@ -1,3 +1,103 @@
+<?php require_once('Connections/connkereta.php'); ?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? "'" . doubleval($theValue) . "'" : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+$editFormAction = $_SERVER['PHP_SELF'];
+if (isset($_SERVER['QUERY_STRING'])) {
+  $editFormAction .= "?" . htmlentities($_SERVER['QUERY_STRING']);
+}
+
+if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "form1")) {
+  $insertSQL = sprintf("INSERT INTO users (username, password) VALUES (%s, %s)",
+                       GetSQLValueString($_POST['username'], "text"),
+                       GetSQLValueString($_POST['password'], "text"));
+
+  mysql_select_db($database_connkereta, $connkereta);
+  $Result1 = mysql_query($insertSQL, $connkereta) or die(mysql_error());
+
+  $insertGoTo = "register_success.php";
+  if (isset($_SERVER['QUERY_STRING'])) {
+    $insertGoTo .= (strpos($insertGoTo, '?')) ? "&" : "?";
+    $insertGoTo .= $_SERVER['QUERY_STRING'];
+  }
+  header(sprintf("Location: %s", $insertGoTo));
+}
+
+mysql_select_db($database_connkereta, $connkereta);
+$query_rsusers = "SELECT * FROM users";
+$rsusers = mysql_query($query_rsusers, $connkereta) or die(mysql_error());
+$row_rsusers = mysql_fetch_assoc($rsusers);
+$totalRows_rsusers = mysql_num_rows($rsusers);
+?><?php
+// *** Validate request to login to this site.
+if (!isset($_SESSION)) {
+  session_start();
+}
+
+$loginFormAction = $_SERVER['PHP_SELF'];
+if (isset($_GET['accesscheck'])) {
+  $_SESSION['PrevUrl'] = $_GET['accesscheck'];
+}
+
+if (isset($_POST['text1'])) {
+  $loginUsername=$_POST['text1'];
+  $password=$_POST['text2'];
+  $MM_fldUserAuthorization = "accessLevel";
+  $MM_redirectLoginSuccess = "user.php";
+  $MM_redirectLoginFailed = "fail.php";
+  $MM_redirecttoReferrer = false;
+  mysql_select_db($database_connkereta, $connkereta);
+  	
+  $LoginRS__query=sprintf("SELECT username, password, accessLevel FROM users WHERE username=%s AND password=%s",
+  GetSQLValueString($loginUsername, "text"), GetSQLValueString($password, "text")); 
+   
+  $LoginRS = mysql_query($LoginRS__query, $connkereta) or die(mysql_error());
+  $loginFoundUser = mysql_num_rows($LoginRS);
+  if ($loginFoundUser) {
+    
+    $loginStrGroup  = mysql_result($LoginRS,0,'accessLevel');
+    
+    //declare two session variables and assign them
+    $_SESSION['MM_Username'] = $loginUsername;
+    $_SESSION['MM_UserGroup'] = $loginStrGroup;	      
+
+    if (isset($_SESSION['PrevUrl']) && false) {
+      $MM_redirectLoginSuccess = $_SESSION['PrevUrl'];	
+    }
+    header("Location: " . $MM_redirectLoginSuccess );
+  }
+  else {
+    header("Location: ". $MM_redirectLoginFailed );
+  }
+}
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -142,3 +242,6 @@ delay:3000//Delay between transitions
 	</div>
 </body> 
 </html>
+<?php
+mysql_free_result($rsusers);
+?>
