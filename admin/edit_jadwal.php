507057a1e72da90216b666f49d5c30417bb3a4e9
maulanaaga <agamaulana.informatika@gmail.com>
2016-06-11 03:17:54 +0700

    update (ganti format tanggal)

--- admin/edit_jadwal.php
+++ admin/edit_jadwal.php
@@ -211,8 +211,8 @@ $queryString_rsjadwal = sprintf("&totalRows_rsjadwal=%d%s", $totalRows_rsjadwal,
           <p><img src="../images/footer-l2.png" alt="" width="265" height="55" longdesc="images/footer-l2.png" /></p>
             <p><?php 
 $nextWeek = time() + (7*24*60*60);//7days;24 hours;60 mins;60 secs
-echo 'SEKARANG: '.date('Y-m-d')."\n";
-echo 'MINGGU DEPAN:'.date('Y-m-d',$nextWeek)."\n";
+echo 'SEKARANG: '.date('d-m-Y')."\n";
+echo 'MINGGU DEPAN:'.date('d-m-Y',$nextWeek)."\n";
 ?>
   </h10></p>
             <p class="style1">Records <?php echo ($startRow_rsjadwal + 1) ?> to <?php echo min($startRow_rsjadwal + $maxRows_rsjadwal, $totalRows_rsjadwal) ?> of <?php echo $totalRows_rsjadwal ?> </p>
