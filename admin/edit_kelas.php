76de21bcd0ef37b0f8b1f0736684e5ed39f4ede7
maulanaaga <agamaulana.informatika@gmail.com>
2016-06-11 16:59:48 +0700

    update edit kelas

--- admin/edit_kelas.php
+++ admin/edit_kelas.php
@@ -212,7 +212,7 @@ $queryString_rsjadwal = sprintf("&totalRows_rsjadwal=%d%s", $totalRows_rsjadwal,
 <div id="container">
 	<div id="header">
     	<h1><a href="/"><img src="../images/footer-l.png" alt="" width="94" height="89" longdesc="images/200px-PT.KA.svg.png" /></a></h1>
-      <h2>Welcome to ticket On-Line Kereta Api</h2>
+      <h2>Welcome to GO-L300</h2>
         <div class="clear"></div>
     </div>
 <div id="nav">
@@ -224,8 +224,8 @@ $queryString_rsjadwal = sprintf("&totalRows_rsjadwal=%d%s", $totalRows_rsjadwal,
     </ul>
   </div>
     <div id="page-intro">
-    	<h2>Welcome to ticket On-Line Kereta Api</h2>
-        <p>Membuat kemudahan dan kenyamanan dalam pemesanan tiket kereta api, "NO COPET, NO NGANTRI, NO CALO"</p>
+    	<h2>Welcome to ticket GO-L300</h2>
+        <p>Membuat kemudahan dan kenyamanan dalam pemesanan tiket L300, "NO REPOT, NO NGANTRI, NO CALO"</p>
         <p>&nbsp;</p>
   </div>
     <div id="body">
@@ -234,8 +234,8 @@ $queryString_rsjadwal = sprintf("&totalRows_rsjadwal=%d%s", $totalRows_rsjadwal,
           <p><img src="../images/footer-l2.png" alt="" width="265" height="55" longdesc="images/footer-l2.png" /></p>
             <p align="left"><?php 
 $nextWeek = time() + (7*24*60*60);//7days;24 hours;60 mins;60 secs
-echo 'SEKARANG: '.date('Y-m-d')."\n";
-echo 'MINGGU DEPAN:'.date('Y-m-d',$nextWeek)."\n";
+echo 'SEKARANG: '.date('d-m-Y')."\n";
+echo 'MINGGU DEPAN:'.date('d-m-Y',$nextWeek)."\n";
 ?></p>
             <form action="<?php echo $editFormAction; ?>" method="post" id="form1">
               <table>
@@ -267,7 +267,7 @@ echo 'MINGGU DEPAN:'.date('Y-m-d',$nextWeek)."\n";
     </div>
 </div>
 <div id="footer">
-	<p>&copy; keretaonline 2010. Website Design by <a href="http://www.spyka.net">Free CSS Templates</a> | <a href="http://www.justfreetemplates.com">Free Web Templates</a> modified By:Alter</p>
+	<p>&copy; GO-L300 KELOMPOK 6 RPL</p>
 </div>
 </body>
 </html>
