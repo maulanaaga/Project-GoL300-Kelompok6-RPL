ca9f9c72f367c8d20004f77dd511774d5fb7788f
Mukhsin Arafat <mukhsin@outlook.com>
2016-06-11 23:27:52 +0700

    buat konfirmasi sukses mendaftar

---
+++ register_success.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Selamat Datang di GO-L300</title>
+<link rel="Shortcut Icon" href="images/train.ico">
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+<script type="text/javascript" src="js/jquery00.js"></script> 
+<script type="text/javascript" src="js/jquery01.js"></script>		
+<script type="text/javascript" src="js/coin-sli.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){$('#coin-slider').coinslider({ 
+width:900,height:300,navigation:true,effect:'random',//random, swirl, rain, straight
+delay:3000//Delay between transitions
+});});
+</script>
+<script type="text/javascript" src="js/function.js"></script>
+<style type="text/css">
+<!--
+.style2 {
+	color: #FF0000;
+	font-weight: bold;
+}
