c18c94134cd4672b723a8b6e4bf7c0c0181c6ab4
Mukhsin Arafat <mukhsin@outlook.com>
2016-06-12 00:49:54 +0700

    buat file notifikasi pengguna gagal melakukan pendaftaran

---
+++ fail.php
@@ -0,0 +1,84 @@
+<?php require_once('Connections/connkereta.php'); ?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? "'" . doubleval($theValue) . "'" : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+mysql_select_db($database_connkereta, $connkereta);
+$query_rsusers = "SELECT * FROM users";
+$rsusers = mysql_query($query_rsusers, $connkereta) or die(mysql_error());
+$row_rsusers = mysql_fetch_assoc($rsusers);
+$totalRows_rsusers = mysql_num_rows($rsusers);
+?><?php
+// *** Validate request to login to this site.
+if (!isset($_SESSION)) {
+  session_start();
+}
+
+$loginFormAction = $_SERVER['PHP_SELF'];
+if (isset($_GET['accesscheck'])) {
+  $_SESSION['PrevUrl'] = $_GET['accesscheck'];
+}
+
+if (isset($_POST['text1'])) {
+  $loginUsername=$_POST['text1'];
+  $password=$_POST['text2'];
+  $MM_fldUserAuthorization = "accessLevel";
+  $MM_redirectLoginSuccess = "user.php";
+  $MM_redirectLoginFailed = "fail.php";
+  $MM_redirecttoReferrer = false;
+  mysql_select_db($database_connkereta, $connkereta);
+  	
+  $LoginRS__query=sprintf("SELECT username, password, accessLevel FROM users WHERE username=%s AND password=%s",
+  GetSQLValueString($loginUsername, "text"), GetSQLValueString($password, "text")); 
+   
+  $LoginRS = mysql_query($LoginRS__query, $connkereta) or die(mysql_error());
+  $loginFoundUser = mysql_num_rows($LoginRS);
+  if ($loginFoundUser) {
+    
+    $loginStrGroup  = mysql_result($LoginRS,0,'accessLevel');
+    
+    //declare two session variables and assign them
+    $_SESSION['MM_Username'] = $loginUsername;
+    $_SESSION['MM_UserGroup'] = $loginStrGroup;	      
+
+    if (isset($_SESSION['PrevUrl']) && false) {
+      $MM_redirectLoginSuccess = $_SESSION['PrevUrl'];	
+    }
+    header("Location: " . $MM_redirectLoginSuccess );
+  }
+  else {
+    header("Location: ". $MM_redirectLoginFailed );
+  }
+}
+?>
+
+
+<?php
+mysql_free_result($rsusers);
+?>
