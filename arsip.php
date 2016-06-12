1c6d3adc451ce4b6a846ea957a522b3c1a49a0f2
Rizki Putri Arbani <rizkiputriarbani@gmail.com>
2016-06-12 14:16:42 -0700

    halaman arsip

---
+++ arsip.php
@@ -0,0 +1,93 @@
+ <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Member Area</title><script>  
+function confirmLogout() {  
+        return confirm("Yakin Logout?")  
+      }  
+</script><link rel="Shortcut Icon" href="images/train.ico">  
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+<script type="text/javascript" src="js/jquery00.js"></script> 
+<script type="text/javascript" src="js/jquery01.js"></script>		
+<script type="text/javascript" src="js/coin-sli.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){$('#coin-slider').coinslider({ 
+width:900,height:300,navigation:true,effect:'random',//random, swirl, rain, straight
+delay:3000//Delay between transitions
+});});
+</script>
+<script type="text/javascript" src="js/function.js"></script>
+</head>
+<body class="noJs">
+	<div class="container">
+		<div id="header">
+			<div class="wrap">
+				<div class="logo">
+					<a href="user.php" title="Design Lovers">Design Lovers</a>				</div>
+		  <ul id="nav">
+					<li class="current"><a href="user.php">Home</a></li>
+				  <li><a href="jadwal.php">JADWAL</a>					</li>
+		    <li><a href="berita.php">BERITA</a></li>
+			<li><a href="arsip.php">ARSIP</a></li>
+			<li><a href="contact.php">Contact</a></li>
+			  </ul>
+			</div>
+		</div>
+		<div class="separator"></div>
+		<div class="wrap">
+				<div class="main intro">Membuat kemudahan dan kenyamanan dalam pemesanan tiket kereta api, &quot;NO COPET, NO NGANTRI, NO CALO&quot;</div>
+				
+  <div class="sidebar" id="search_box">
+    <label for="search">Input Tujuan Anda...</label>
+                    <input type="submit" value="" id="search_btn" class="submit" />
+  </div>
+	  </div>
+			<div class="separator"></div>
+			<div class="wrap">
+			<div id="main" class="main">
+				<div class="headline">
+				  <h1>Peta Jalur Transportasi Aceh</h1>
+				  <p><img src="peta.jpg" width="600" height="400" longdesc="images/peta_jawa.png" /></p>
+				  <p>Jalur - Jalur Trasnporasi yang dilewati berbagai transportasi dari darat, laut dan juga udara, inilah rute ataupun jalan yang di lewati setiap transportasi seperti pada gambar di atas.</p>
+				  <br>
+				  <p><img src="petaA.gif" width="600" height="400" longdesc="images/peta_sumatera.png" /></p>
+					
+			  </div>
+				</div>
+			<div id="sidebar" class="sidebar">
+				<h2>Layanan Kami</h2>
+				<ul class="arrow_list">
+					<li>Pesan Tiket</li>
+					<li>Tracking Tiket</li>
+					<li>Promo</li>
+				</ul>
+				
+	  <div class="separator"></div>
+				
+	  <h2>&nbsp;</h2>
+				<p>&nbsp;</p>
+</div>
+	  </div>
+	</div>
+	<div id="footer">
+		<div class="container">
+			<div class="wrap">
+				<div class="footer_logo">
+					<a href="user.php" title="Design Lovers">Design Lovers</a>				</div>
+		  <ul id="footer_nav">
+					<li class="current"><a href="user.php">Home</a></li>
+				  <li><a href="jadwal.php">JADWAL</a></li>
+			<li><a href="berita.php">BERITA</a></li>
+			<li><a href="arsip.php">ARSIP</a></li>
+			<li><a href="contact.php">Contact</a></li>
+			  </ul>
+				<p id="copy">Designed by &copy;alter</p>
+			</div>
+		</div>
+		<div class="clear"></div>
+	</div>
+</body> 
+</html>
