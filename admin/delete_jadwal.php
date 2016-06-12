6f4774d5c9b35e39d2b7c45cd7c0e2b45ba208a5
maulanaaga <agamaulana.informatika@gmail.com>
2016-06-12 12:44:59 +0700

    update link

--- admin/delete_jadwal.php
+++ admin/delete_jadwal.php
@@ -57,6 +57,7 @@ $totalRows_rsjadwal = mysql_num_rows($rsjadwal);
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="Shortcut Icon" href="images/train.ico">
 <title>DELETE JADWAL</title>
 </head>
 
