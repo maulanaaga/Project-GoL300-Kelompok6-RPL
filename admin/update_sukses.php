4bd3d501c69fc63c5ee100e79f9c325110c9cfbb
maulanaaga <agamaulana.informatika@gmail.com>
2016-06-12 13:24:53 +0700

    update tambah link gambar

--- admin/update_sukses.php
+++ admin/update_sukses.php
@@ -169,7 +169,7 @@ $queryString_rsjadwal = sprintf("&totalRows_rsjadwal=%d%s", $totalRows_rsjadwal,
 <script>function confirmLogout() {  
         return confirm("Yakin Logout?")  
       }  </script>
-<link rel="Shortcut Icon" href="../images/train.ico">
+<link rel="Shortcut Icon" href="images/icon.ico">
 <link rel="stylesheet" href="../styles.css" type="text/css" />
 <style type="text/css">
 <!--
@@ -186,7 +186,7 @@ $queryString_rsjadwal = sprintf("&totalRows_rsjadwal=%d%s", $totalRows_rsjadwal,
 <body>
 <div id="container">
 	<div id="header">
-    	<h1><a href="/"><img src="../images/footer-l.png" alt="" width="94" height="89" longdesc="images/200px-PT.KA.svg.png" /></a></h1>
+    	<h1><a href="/"><img src="images/Go1.png" alt="" width="94" height="89" longdesc="images/200px-PT.KA.svg.png" /></a></h1>
       <h2>Welcome to GO-L300</h2>
         <div class="clear"></div>
     </div>
@@ -206,7 +206,7 @@ $queryString_rsjadwal = sprintf("&totalRows_rsjadwal=%d%s", $totalRows_rsjadwal,
     <div id="body">
 		<div id="content">
 			<h2><strong>UPDATE DATA</strong></h2>
-          <p><img src="../images/footer-l2.png" alt="" width="265" height="55" longdesc="images/footer-l2.png" /></p>
+          <p><img src="images/Go2.png" alt="" width="265" height="55" longdesc="images/footer-l2.png" /></p>
             <p align="left"><?php 
 $nextWeek = time() + (7*24*60*60);//7days;24 hours;60 mins;60 secs
 echo 'SEKARANG: '.date('d-m-Y')."\n";
